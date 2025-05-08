470908e55470e99b2621f2464fdcce5b2f375f28
Ashita  D Jawali <113736983+Ashita2619@users.noreply.github.com>
2025-05-08 09:43:28 -0500

    Update WF_2_helper.py

--- scripts/WF_2_DB/WF_2_helper.py
+++ scripts/WF_2_DB/WF_2_helper.py
@@ -153,6 +153,6 @@ class demographics_import():
 
 if __name__ == "__main__":
     
-    import_demo = demographics_import("/home/ssh_user/Documents/GitHub/CRAB_Analysis")
-    sample_hsn = import_demo.get_lims_demographics(['2434975','2445821','2468507','2488768','2492075','2506355','2510743','2527973'],"111323","/home/ssh_user/WGS_Drive/CRAB_WGS_Sequencing")
+    import_demo = demographics_import("/epi/home/ssh_user/Documents/GitHub/CRAB_Analysis")
+    sample_hsn = import_demo.get_lims_demographics(['2434975','2445821','2468507','2488768','2492075','2506355','2510743','2527973'],"111323","/epi/home/ssh_user/WGS_Drive/CRAB_WGS_Sequencing")
         
