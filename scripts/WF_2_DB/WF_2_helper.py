26a672789335ab60b34489d9309320b8d14a53aa
Ashita  D Jawali <113736983+Ashita2619@users.noreply.github.com>
2026-01-27 08:52:50 -0600

    Update WF_2_helper.py
    Changed the working directory.

--- scripts/WF_2_DB/WF_2_helper.py
+++ scripts/WF_2_DB/WF_2_helper.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(0,'/epi/home/ssh_user/Documents/GitHub/Waste-Water/scripts')
+sys.path.insert(0,'/epi/home/ashita.jawali@kdhe.state.ks.us/Documents/GitHub/Waste-Water/scripts')
 from ms_sql_handler import ms_sql_handler
 import pandas as pd
 import cx_Oracle as co
@@ -160,6 +160,6 @@ class demographics_import():
 if __name__ == "__main__":
     
     import_demo = demographics_import("/epi/home/ashita.jawali@kdhe.state.ks.us/Documents/GitHub/Waste-Water")
-    sample_hsn = import_demo.get_lims_demographics(['2434975','2445821','2468507','2488768','2492075','2506355','2510743','2527973'],"111323","/epi/home/ssh_user/WGS_Drive/CRAB_WGS_Sequencing")
+    sample_hsn = import_demo.get_lims_demographics(['2434975','2445821','2468507','2488768','2492075','2506355','2510743','2527973'],"111323","/epi/home/ashita.jawali@kdhe.state.ks.us/WGS_Drive/CRAB_WGS_Sequencing")
 
         
